5d0ee26beee0c8744b76f332d30519fe84d46cb4
SanjeevMakesh24 <sanjeevmakesh24@gmail.com>
2025-04-20 16:49:53 -0500

    connected to openai

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.schema import Document
+from dotenv import load_dotenv
+import os
+from langchain_community.llms import OpenAI
+from langchain_openai import OpenAI
+from langchain_core.prompts import PromptTemplate
 
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+#to load variables from .env file
+load_dotenv()
+
+llm = OpenAI(temperature=0.2)
 
 #function to extract video ID from URL
 def extract_video_id(url):
@@ -17,7 +28,7 @@ def extract_video_id(url):
     return url.split("v=")[-1].split("&")[0]
 
 #video link
-video_link = "https://www.youtube.com/watch?v=jmrHBHJTVZ4&t=2492s"  
+video_link = "https://www.youtube.com/watch?v=Kbk9BiPhm7o&t=1s"  
 video_id = extract_video_id(video_link)
 
 try:
@@ -62,8 +73,8 @@ try:
     vectorstore = FAISS.from_documents(docs, embedding_model)
 
 
-    query = "What do they talk about Apple products, ipads, iphones?"
-    relevent_docs = vectorstore.similarity_search(query, k=3) #list
+    question = "What do they talk about neurosurgery?"
+    relevent_docs = vectorstore.similarity_search(question, k=3) #list
 
     #print(type(relevent_docs))
 
@@ -76,6 +87,46 @@ try:
         print(doc.page_content)
 
     
+    #prompt template
+    prompt = PromptTemplate.from_template("""
+    You are an AI assistant helping summarize key points from a YouTube transcript.
+
+    Use the context below to answer the question thoroughly and accurately, reflecting what was actually said in the video.
+
+    Context:
+    {context}
+
+    Question: {question}
+                                          
+    INSTRUCTIONS:
+    1. Focus exclusively on information mentioned in the transcript
+    2. Provide direct quotes when possible to support key points
+    3. Maintain the original meaning and nuance from the video
+    4. If the transcript doesn't contain relevant information to answer the question, clearly state this
+    5. Organize your response with clear structure and headings if appropriate
+    6. If you need more context from the video, here is the video link {link}
+
+    Answer:
+    """)
+
+    context = "\n\n".join([doc.page_content for doc in relevent_docs])
+
+    chain = prompt | llm
+
+    # Run the LLM to get a final answer
+    response = chain.invoke({
+        "context": context,
+        "question": question,
+        "link": video_link
+    })
+
+    print("\n Final Answer:")
+    print(response)
+    
 
 except Exception as e:
     print(f"Error: {e}")
+
+
+#scope for improvements:
+#longer videos --> more chunks to the llm
